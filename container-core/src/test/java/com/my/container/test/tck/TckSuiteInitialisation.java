9063e3b815a9fa90a94b713a8e86048f729931ee
kevin pollet <pollet.kevin@gmail.com>
2011-01-18 20:18:46 +0100

    Add JSR 330 tck test suite in build.

--- container-core/src/test/java/com/my/container/test/tck/TckSuiteInitialisation.java
+++ container-core/src/test/java/com/my/container/test/tck/TckSuiteInitialisation.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2011 Kevin Pollet
+ * Copyright 2010 Kevin Pollet
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,13 +15,29 @@
  */
 package com.my.container.test.tck;
 
+import com.my.container.context.ApplicationContext;
+import com.my.container.context.Context;
+import junit.framework.Test;
+import org.atinject.tck.Tck;
+import org.atinject.tck.auto.Car;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 /**
+ * JSR-330 TCK.
+ * 
  * @author Kevin Pollet
  */
-@RunWith( Suite.class )
-@Suite.SuiteClasses({ TckSuiteInitialisation.class })
-public class TckSuiteTest {
+public class TckSuiteInitialisation {
+
+    /**
+     * Configure and return the suite.
+     * @return the suite.
+     */
+    public static Test suite() {
+        Context context = new ApplicationContext( new TckBindingProvider() );
+        Car car = context.getBean( Car.class );
+        return Tck.testsFor( car, true, true );
+    }
+
 }
