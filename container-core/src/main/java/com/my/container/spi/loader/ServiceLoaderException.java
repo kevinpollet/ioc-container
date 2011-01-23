2c66366b444507faed13277901e88fc5ae143963
kevin pollet <pollet.kevin@gmail.com>
2011-01-23 17:40:49 +0100

    Format ServiceLoaderException.

--- container-core/src/main/java/com/my/container/spi/loader/ServiceLoaderException.java
+++ container-core/src/main/java/com/my/container/spi/loader/ServiceLoaderException.java
@@ -19,28 +19,28 @@ package com.my.container.spi.loader;
  * This exception was thrown when service
  * loader encounter a problem during loading.
  *
- * @author kevinpollet
+ * @author Kevin Pollet
  */
 public class ServiceLoaderException extends RuntimeException {
 
-    /**
-     * {@inheritDoc}
-     */
-    public ServiceLoaderException(final String message) {
-        super(message);
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	public ServiceLoaderException(final String message) {
+		super( message );
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    public ServiceLoaderException(final String message,final Throwable cause) {
-        super(message, cause);
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	public ServiceLoaderException(final String message, final Throwable cause) {
+		super( message, cause );
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    public ServiceLoaderException(final Throwable cause) {
-        super(cause);
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	public ServiceLoaderException(final Throwable cause) {
+		super( cause );
+	}
 }
